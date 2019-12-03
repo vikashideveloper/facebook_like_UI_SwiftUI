702c0459bfee205866ab508b22638b3501f00076
Vikash Kumar <vikash.ideveloper@gmail.com>
2019-12-03 23:47:04 +0530

    tab bar added

---
+++ FacebookUIDemo/FBMainTabsView.swift
@@ -0,0 +1,21 @@
+//
+//  FBMainTabsView.swift
+//  FacebookUIDemo
+//
+//  Created by Vikash on 03/12/19.
+//  Copyright © 2019 Vikash Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct FBMainTabsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FBMainTabsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FBMainTabsView()
+    }
+}
